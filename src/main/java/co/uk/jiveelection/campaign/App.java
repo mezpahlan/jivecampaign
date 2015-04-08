d431b70ca97bfbc39f2e12adb9766d6277af9451
Mez Pahlan <mez.pahlan@gmail.com>
2015-04-08 07:13:28 +0100

    Added short Javadoc

--- src/main/java/co/uk/jiveelection/campaign/App.java
+++ src/main/java/co/uk/jiveelection/campaign/App.java
@@ -7,16 +7,17 @@ import co.uk.jiveelection.campaign.jive.JiveHelper;
 import co.uk.jiveelection.campaign.twit.TwitHelper;
 
 /**
- * Hello world!
+ * Main entry point for the Jive Campaign application. Sets up a continuously running loop that
+ * checks the latest tweet from the named candidate, translates it to jive, then tweets it as a
+ * jivebot.
  *
  */
 public class App {
 
-	
 	public static void main(String[] args) throws TwitterException, IOException {
 		TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", "davidjiveron");
-//		TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", "edjiveaband");
-//		TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", "nickjiveegg");
-	  }
-		
+		// TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", "edjiveaband");
+		// TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", "nickjiveegg");
+	}
+
 }
