d01989213925898343276219fdf7aa0245577b68
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-14 17:15:23 +0100

    Convert to Stream Listener
    Now we don't have to worry about keeping track of ids.

--- src/main/java/co/uk/jiveelection/campaign/App.java
+++ src/main/java/co/uk/jiveelection/campaign/App.java
@@ -10,7 +10,6 @@ import co.uk.jiveelection.campaign.twit.TwitHelper;
  * Main entry point for the Jive Campaign application. Sets up a continuously running loop that
  * checks the latest tweet from the named candidate, translates it to jive, then tweets it as a
  * jivebot.
- *
  */
 @SpringBootApplication
 public class App {
@@ -18,9 +17,9 @@ public class App {
 	public static void main(String[] args) throws TwitterException, IOException,
 			InterruptedException {
 
-		TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", "davidjiveron");
-		TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", "edjiveaband");
-		TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", "nickjivegg");
+		TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", "David_Jiveron", TwitConfig.DAVID_JIVERON_ACCESS_TOKEN, TwitConfig.DAVID_JIVERON_ACCESS_TOKEN_SECRET);
+		TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", "Ed_Jiveaband", TwitConfig.ED_JIVEABAND_ACCESS_TOKEN, TwitConfig.ED_JIVEABAND_ACCESS_TOKEN_SECRET);
+		TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", "nick_jivegg", TwitConfig.NICK_JIVEGG_ACCESS_TOKEN, TwitConfig.NICK_JIVEGG_ACCESS_TOKEN_SECRET);
 
 	}
 
