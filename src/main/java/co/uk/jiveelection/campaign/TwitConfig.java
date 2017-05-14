6c73d51f83e8dd8b546b424706af6744c9a2f4ae
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-14 17:21:51 +0100

    Add Consumer token placeholders

--- src/main/java/co/uk/jiveelection/campaign/TwitConfig.java
+++ src/main/java/co/uk/jiveelection/campaign/TwitConfig.java
@@ -6,6 +6,10 @@ package co.uk.jiveelection.campaign;
  * Placeholder in SCM, this file needs to be correctly filled out prior to launch.
  */
 public class TwitConfig {
+    // Add your applications tokens here
+    public static final String CONSUMER_TOKEN = "";
+    public static final String CONSUMER_TOKEN_SECRET = "";
+
     // Update this pair for each of your Jive Bots
     public static final String JIVEBOT_ACCESS_TOKEN = "";
     public static final String JIVEBOT_ACCESS_TOKEN_SECRET = "";
