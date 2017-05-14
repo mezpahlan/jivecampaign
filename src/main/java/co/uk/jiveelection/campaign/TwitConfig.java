470e7ce9886863b2e6e0b4766bf10aaecf94e7d4
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-14 13:52:56 +0100

    Add placeholder configuration file
    This will be untracked so that we don't leak secrets.

---
+++ src/main/java/co/uk/jiveelection/campaign/TwitConfig.java
@@ -0,0 +1,12 @@
+package co.uk.jiveelection.campaign;
+
+/**
+ * Configuration for the Twitter Jive Bots.
+ *
+ * Placeholder in SCM, this file needs to be correctly filled out prior to launch.
+ */
+public class TwitConfig {
+    // Update this pair for each of your Jive Bots
+    public static final String JIVEBOT_ACCESS_TOKEN = "";
+    public static final String JIVEBOT_ACCESS_TOKEN_SECRET = "";
+}
