b3c15bbf8a717c4cd56b7c75d975ccfd41cf0c3a
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-15 22:47:45 +0100

    Add GE2017 bots

--- src/main/java/co/uk/jiveelection/campaign/App.java
+++ src/main/java/co/uk/jiveelection/campaign/App.java
@@ -20,11 +20,19 @@ public class App {
 
         SpringApplication.run(App.class, args);
 
+        // GE2015
         TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", TwitConfig.DAVID_JIVERON_ACCESS_TOKEN, TwitConfig.DAVID_JIVERON_ACCESS_TOKEN_SECRET);
         TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", TwitConfig.ED_JIVEABAND_ACCESS_TOKEN, TwitConfig.ED_JIVEABAND_ACCESS_TOKEN_SECRET);
         TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", TwitConfig.NICK_JIVEGG_ACCESS_TOKEN, TwitConfig.NICK_JIVEGG_ACCESS_TOKEN_SECRET);
+
+        // GE2017
         TwitHelper jivebotTheresaMay = new TwitHelper("theresa_may", TwitConfig.JIVERESA_MAY_ACCESS_TOKEN, TwitConfig.JIVERESA_MAY_ACCESS_TOKEN_SECRET);
         TwitHelper jivebotJeremyCorbyn = new TwitHelper("jeremycorbyn", TwitConfig.JEREMY_JIVEBYN_ACCESS_TOKEN, TwitConfig.JEREMY_JIVEBYN_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotJiveFarron = new TwitHelper("timfarron", TwitConfig.JIVE_FARRON_ACCESS_TOKEN, TwitConfig.JIVE_FARRON_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotJiveolaSturgeon = new TwitHelper("NicolaSturgeon", TwitConfig.JIVEOLA_STURGEON_ACCESS_TOKEN, TwitConfig.JIVEOLA_STURGEON_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotPaulJiveallJivekip = new TwitHelper("paulnuttallukip", TwitConfig.PAUL_JIVEALL_ACCESS_TOKEN, TwitConfig.PAUL_JIVEALL_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotJiveanneWood = new TwitHelper("LeanneWood", TwitConfig.JIVEANNE_WOOD_ACCESS_TOKEN, TwitConfig.JIVEANNE_WOOD_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotJiveolineLucas = new TwitHelper("CarolineLucas", TwitConfig.JIVEOLINE_LUCAS_ACCESS_TOKEN, TwitConfig.JIVEOLINE_LUCAS_ACCESS_TOKEN_SECRET);
     }
 
 }
