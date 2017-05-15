a5734b02e74650325b6a5594e1dff59f884e33d2
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-15 08:09:25 +0100

    Add Theresa May Jive bot

--- src/main/java/co/uk/jiveelection/campaign/App.java
+++ src/main/java/co/uk/jiveelection/campaign/App.java
@@ -1,10 +1,11 @@
 package co.uk.jiveelection.campaign;
 
-import java.io.IOException;
-
+import co.uk.jiveelection.campaign.twit.TwitHelper;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import twitter4j.TwitterException;
-import co.uk.jiveelection.campaign.twit.TwitHelper;
+
+import java.io.IOException;
 
 /**
  * Main entry point for the Jive Campaign application. Sets up a continuously running loop that
@@ -14,13 +15,15 @@ import co.uk.jiveelection.campaign.twit.TwitHelper;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) throws TwitterException, IOException,
-			InterruptedException {
+    public static void main(String[] args) throws TwitterException, IOException,
+            InterruptedException {
 
-		TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", "David_Jiveron", TwitConfig.DAVID_JIVERON_ACCESS_TOKEN, TwitConfig.DAVID_JIVERON_ACCESS_TOKEN_SECRET);
-		TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", "Ed_Jiveaband", TwitConfig.ED_JIVEABAND_ACCESS_TOKEN, TwitConfig.ED_JIVEABAND_ACCESS_TOKEN_SECRET);
-		TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", "nick_jivegg", TwitConfig.NICK_JIVEGG_ACCESS_TOKEN, TwitConfig.NICK_JIVEGG_ACCESS_TOKEN_SECRET);
+        SpringApplication.run(App.class, args);
 
-	}
+        TwitHelper jivebotDavidCameron = new TwitHelper("David_Cameron", TwitConfig.DAVID_JIVERON_ACCESS_TOKEN, TwitConfig.DAVID_JIVERON_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotEdMiliband = new TwitHelper("Ed_Miliband", TwitConfig.ED_JIVEABAND_ACCESS_TOKEN, TwitConfig.ED_JIVEABAND_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotNickClegg = new TwitHelper("nick_clegg", TwitConfig.NICK_JIVEGG_ACCESS_TOKEN, TwitConfig.NICK_JIVEGG_ACCESS_TOKEN_SECRET);
+        TwitHelper jivebotTheresaMay = new TwitHelper("theresa_may", TwitConfig.JIVERESA_MAY_ACCESS_TOKEN, TwitConfig.JIVERESA_MAY_ACCESS_TOKEN_SECRET);
+    }
 
 }
