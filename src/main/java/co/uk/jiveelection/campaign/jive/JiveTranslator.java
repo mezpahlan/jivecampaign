5b3950d77ce630bc807b47d4b44e91aa3e28e453
Mez Pahlan <mez.pahlan@gmail.com>
2017-05-27 21:42:29 +0100

    Add JiveTranslator type

---
+++ src/main/java/co/uk/jiveelection/campaign/jive/JiveTranslator.java
@@ -0,0 +1,8 @@
+package co.uk.jiveelection.campaign.jive;
+
+/**
+ * Describes the operations afforded to Jive Translators.
+ */
+public interface JiveTranslator {
+    String translate(String text);
+}
