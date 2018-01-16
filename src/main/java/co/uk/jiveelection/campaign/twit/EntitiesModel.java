55808040cd94685998908708518a5dbddf5ea793
Mez Pahlan <mez.pahlan@gmail.com>
2018-01-16 22:25:28 +0000

    Remove unused properties of EntitiesModel

--- src/main/java/co/uk/jiveelection/campaign/twit/EntitiesModel.java
+++ src/main/java/co/uk/jiveelection/campaign/twit/EntitiesModel.java
@@ -1,46 +1,25 @@
 package co.uk.jiveelection.campaign.twit;
 
-public class EntitiesModel implements Comparable<EntitiesModel>{
-	private int start;
-	private int end;
-	private String text;
-	private String type;
-	
-	public int getStart() {
-		return start;
-	}
-	public void setStart(int start) {
-		this.start = start;
-	}
-	public int getEnd() {
-		return end;
-	}
-	public void setEnd(int end) {
-		this.end = end;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	
-	public EntitiesModel(int start, int end, String text, String type) {
-		this.start = start;
-		this.end = end;
-		this.text = text;
-		this.type = type;
-	}
-	
-	@Override
-	public int compareTo(EntitiesModel o) {
-		return this.getStart() - o.getStart();
-	}
+public class EntitiesModel implements Comparable<EntitiesModel> {
+    private int start;
+    private int end;
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public EntitiesModel(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public int compareTo(EntitiesModel o) {
+        return this.getStart() - o.getStart();
+    }
 
 }
--- src/main/java/co/uk/jiveelection/campaign/twit/TweetHelper.java
+++ src/main/java/co/uk/jiveelection/campaign/twit/TweetHelper.java
@@ -55,30 +55,26 @@ public class TweetHelper {
         // Get URL Entities
         for (int i = 0; i < status.getURLEntities().length; i++) {
             URLEntity urlEntities = status.getURLEntities()[i];
-            entities.add(new EntitiesModel(urlEntities.getStart(), urlEntities.getEnd(),
-                    urlEntities.getText(), urlEntities.getClass().getName()));
+            entities.add(new EntitiesModel(urlEntities.getStart(), urlEntities.getEnd()));
         }
 
         // Get Media Entities
         for (int i = 0; i < status.getMediaEntities().length; i++) {
             MediaEntity mediaEntities = status.getMediaEntities()[i];
-            entities.add(new EntitiesModel(mediaEntities.getStart(), mediaEntities.getEnd(),
-                    mediaEntities.getText(), mediaEntities.getClass().getName()));
+            entities.add(new EntitiesModel(mediaEntities.getStart(), mediaEntities.getEnd()));
         }
 
         // Get UserMentionEntity if they exists
         for (int i = 0; i < status.getUserMentionEntities().length; i++) {
             UserMentionEntity userMentionEntities = status.getUserMentionEntities()[i];
             entities.add(new EntitiesModel(userMentionEntities.getStart(), userMentionEntities
-                    .getEnd(), userMentionEntities.getText(), userMentionEntities.getClass()
-                    .getName()));
+                    .getEnd()));
         }
 
         // Get HashtagEntity if they exists
         for (int i = 0; i < status.getHashtagEntities().length; i++) {
             HashtagEntity hashTagEntities = status.getHashtagEntities()[i];
-            entities.add(new EntitiesModel(hashTagEntities.getStart(), hashTagEntities.getEnd(),
-                    hashTagEntities.getText(), hashTagEntities.getClass().getName()));
+            entities.add(new EntitiesModel(hashTagEntities.getStart(), hashTagEntities.getEnd()));
         }
 
         // Order the List of Entities by start position
